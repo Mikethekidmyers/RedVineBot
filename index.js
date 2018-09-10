b0e82b389ac58ac00c52d454077a3ce87e02a8a9
Thomas Basso <thomas@kodebyraaet.no>
2018-09-10 14:32:54 +0200

    changed drop command to take additional parameters for miramar, you can now say 'drop miramar north' to get drop points at the north side of the map

--- index.js
+++ index.js
@@ -114,7 +114,6 @@ setInterval( function setPresence(){
 // When chat messages are received
 bot.on("message", function (user, userID, channelID, message, rawEvent)
 {
-    console.log(userID);
     if(userID == '142373989770199040'){
         //if the user is marcus, moves him to the afk channel
         banishPlayer(userID);
@@ -298,31 +297,6 @@ function chooseCaptain(channelID, userID){
     }
 };
 
-//cant get this to work yet, bot joins channel and starts spamming the end stream message..
-
-function summonBot(voiceChannelID){
-    bot.joinVoiceChannel(voiceChannelID, function(error, events){
-        if (error) return console.error(error);
-
-        bot.getAudioContext(voiceChannelID, function(error, stream){
-            if (error) return console.error(error);
-
-            fs.createReadStream('workwork.mp3').pipe(stream, { end: false });
-
-            stream.on('done', function(){
-                console.log('processing finished!');
-            })
-        });
-    });
-};
-
-function dismissBot(voiceChannelID){
-    bot.leaveVoiceChannel(voiceChannelID, function(error, events){
-        if (error) return console.error(error);
-
-    });
-};
-
 function dropZone(channelID, mapName){
     var phraseArray = ["We're heading to ", "We're going to ", "It's hot drop time in ", "It's been a long time since we were in ", "", "Ready, set, ", "Oh how I've missed you ", "Fuck me I hate landing in ", "Let's drop in "];
     var landingPhrase = phraseArray[Math.floor(Math.random() * phraseArray.length)];
@@ -333,33 +307,48 @@ function dropZone(channelID, mapName){
         case "Miramar":
             var miramarArray = ['Alcantara', 'La Cobreria', 'Water Treatment', 'Torre Ahumada', 'Campo Militar', 'Tierra Bronca', 'Cruz Del Valle', 'El Azahar', 'Hacienda del Patron', 'San Martin', 'El Pozo', 'Monte Nuevo', 'Power Grid', 'Graveyard', 'Minas Generales', 'Junkyard', 'Impala', 'La Bendita', 'Pecado', 'Ladrillera', 'Chumacera', 'Los Leones', 'Puerto Paraiso', 'Los Higos', 'Valle del Mar', 'Prison', 'Minas del sur', 'Crater Fields'];
             var rand = miramarArray[Math.floor(Math.random() * miramarArray.length)];
-            bot.sendMessage({
-                to: channelID,
-                message: `${landingPhrase}${rand}`,
-                tts: true,
-            })
+        break;
+
+        case "miramar south":
+        case "Miramar south":
+            var miramarSouthArray = ['Prison', 'Minas Del Sur', 'Valle Del Mar', 'Puerto Paraiso', 'Chumacera', 'Los Leones', 'Los Higos'];
+            var rand = miramarSouthArray[Math.floor(Math.random() * miramarSouthArray.length)];
+        break;
+
+        case "miramar north":
+        case "Miramar north":
+            var miramarNorthArray = ['Alcantara', 'La Cobreria', 'Crater Fields', 'Trailer Park', 'Torre Ahumada', 'Water Treatment', 'Cruz Del Valle', 'Tierra Bronca', 'Campo Militar'];
+            var rand = miramarNorthArray[Math.floor(Math.random() * miramarNorthArray.length)];
+        break;
+
+        case "miramar east":
+        case "Miramar east":
+            var miramarEastArray = ['Torre Ahumada', 'Campo Militar', 'Tierra Bronca', 'Cruz Del Valle', 'El Azahar', 'Junkyard', 'Minar Generales', 'Impala', 'La Bendita', 'Puerto Paraiso', 'Los Leones', 'Islands east of Impala'];
+            var rand = miramarEastArray[Math.floor(Math.random() * miramarEastArray.length)];
+        break;
+
+        case "miramar west":
+        case "Miramar west":
+            var miramarWestArray = ['Prison', 'Minas Del Sur', 'Valle Del Mar', 'Los Higos', 'Chumacera', 'Ladrillera', 'Monte Nuevo', 'El Pozo', 'Trailer Park', 'Alcantara', 'La Cobreria', 'Crater Fields'];
+            var rand = miramarWestArray[Math.floor(Math.random() * miramarWestArray.length)];
+        break;
+
+        case "miramar center":
+        case "Miramar center":
+            var miramarCenterArray = ['La Cobreria', 'Water Treatment', 'San Martin', 'Hacienda Del Patron', 'Cruz Del Valle', 'Power Grid', 'Graveyard', 'Pecado', 'La Bendita', 'Chumacera', 'Los Leones', 'Los Higos'];
+            var rand = miramarCenterArray[Math.floor(Math.random() * miramarCenterArray.length)];
         break;
 
         case "erangel":
         case "Erangel":
             var erangelArray = ['Zharki', 'Shooting Range', 'Severny', 'Stalber', 'Yasnaya Polyana', 'Kameshki', 'Mansion', 'Lipovka', 'Prison', 'Shelter', 'School', 'Mylta Power', 'Rozhok', 'Ruins', 'Mylta', 'Farm', 'Novorepnoye', 'Military Factory', 'Military Police station', 'Military Barracks', 'Military Radio Station', 'Ferry Pier', 'Primorsk', 'Quarry', 'Gatka', 'North Georgopol', 'South Georgopol', 'Pochinki', 'Water Town'];
             var rand = erangelArray[Math.floor(Math.random() * erangelArray.length)];
-            bot.sendMessage({
-                to: channelID,
-                message: `${landingPhrase}${rand}`,
-                tts: true,
-            })
         break;
 
         case "sanhok":
         case "Sanhok":
             var sanhokArray = ['Camp Alpha', 'Ha Tinh', 'Tat Mok', 'Khao', 'Mongnai', 'Paradise Resort', 'Camp Bravo', 'Ruins', 'Bootcamp', 'Lakawi', 'Kampong', 'Quarry', 'Camp Charlie', 'Ban Tai', 'Docks', 'Sahmee', 'Na Kham', 'Tambang', 'Pai Nan', 'Bhan'];
             var rand = sanhokArray[Math.floor(Math.random() * sanhokArray.length)];
-            bot.sendMessage({
-                to: channelID,
-                message: `${landingPhrase}${rand}`,
-                tts: true,
-            })
         break;
 
         default:
@@ -369,6 +358,11 @@ function dropZone(channelID, mapName){
         })
         break;
     }
+    bot.sendMessage({
+        to: channelID,
+        message: `${landingPhrase}${rand}`,
+        tts: true,
+    });
 }
 
 // get season stats
@@ -640,7 +634,6 @@ function getKD(latestMatchID, playerName, channelID){
                     var kills = player.attributes.stats.kills;
                     self.damageDealt = Math.floor(damageDealt);
                     self.kills = kills;
-                    console.log(kills + " kills", "#" + placement, "Damage Dealt: " + self.damageDealt);
                 }
             }
         });
@@ -815,13 +808,6 @@ function getDetailedMatchData(latestMatchID, playerName, channelID){
             rosterDataArray.push(rosterData.id);
         });
 
-        // console.log(rosterDataArray);
-        //
-        // console.log(rosterIdArray);
-        //
-        // console.log(playerIdArray);
-
-
         res.data.included.forEach(function(player){
             if(player.type == 'participant'){
                 var name = player.attributes.stats.name;
@@ -955,10 +941,8 @@ function lastCustom(channelID, playerName, returnID){
 
         if(!returnID){
             getDetailedCustomMatchData(latestMatchID, playerName, channelID);
-            // console.log("getDetailedMatchData");
         } else if(returnID){
             getLastCustomMatch(latestMatchID, playerName, channelID);
-            // console.log("getLastCustomMatch");
         }
 
     })
